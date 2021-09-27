ced78334a3656f3ac41699758b777ac0611a03d3
gusmalc <gustavo@malcotti.com>
2021-09-27 16:38:16 -0300

    1

---
+++ 02_variabili/js/scripts.js
@@ -0,0 +1,57 @@
+/*
+Tema: variabili
+
+Il nome de la variabile:
+    1. non deve coincidere con una delle parole chiave del linguaggio.
+    2. non può iniziare con un numero.
+    3. non può contenere uno spazio.
+    4. non può contenere caratteri speciali come, ad esempio, il punto (.) ed il trattino (-).
+*/
+
+let cita = "Milan";
+let nome = 'Ana'; // string
+let eta = 28; // number
+let studia = true; // boolean [true - false]
+// object
+// undefine
+
+// cita = "Rimini";
+
+
+
+let x = "(2+5)*3";
+
+//let c = parseFloat(campo1) + parseFloat(campo2);
+
+// let c = a.toString() + b.toString();
+
+
+//let y = a%b;
+
+// let y = a**3;
+
+
+//z = z + a;
+
+//z *= 2;
+
+//z += 3;
+
+//a++;
+
+// ++a;
+
+
+
+//console.log(a);
+
+let a = 12;
+let b = 10;
+let z = 2;
+
+
+let f = ( a == b || b == z ) ? "verdadero" : "falso";
+
+// and (&&) - or(||)
+
+console.log(f);
